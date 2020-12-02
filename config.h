6a3224c80e39ff60cf0b7e4083e7af2e936e19b2
mtwb47 <mtwb47@gmail.com>
2020-12-01 23:09:05 -0500

    before dracula

--- config.h
+++ config.h
@@ -6,10 +6,18 @@ static const unsigned int gappx       = 6;      /*   pixel gap between clients *
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=8" };
+static const char *fonts[]          = { "Hack Nerd Font:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
-
-#include "/home/drmdub/.cache/wal/colors-wal-dwm.h"
+static const char col_gray1[]       = "#2a2a2a";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#00a5af";
+static const char *colors[][3]      = {
+/*	               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -50,7 +58,7 @@ static const Layout layouts[] = {
  #include "movestack.c"
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
